e16f11b3ef30d27e71c248c24545f47bc1a94778
masoud habibi <masoud.habibi@metrotrains.com.au>
2023-05-16 13:51:09 +1000

    As I had to setup a windows vs VM at the beginning of the best, i had to mveo files. I didn't commit incrementally!

---
+++ inLogikInterview/LogManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inLogikInterview
+{
+/*
+posting: < user name > -> @< project name > < message >
+reading: < project name >
+following: < user name > follows < project name >
+wall: < user name > wall
+*/
+    internal class LogManager
+    {
+        List<Log> messageLog = new List<Log>();
+        Dictionary<String, List<string>> userProjects = new Dictionary<String, List<string>>();
+        Dictionary<String, List<Log>> userFollowProjectsLogs = new Dictionary<String, List<Log>>();
+        Dictionary<string, List<Log>> projectLogs = new Dictionary<String, List<Log>>();
+        internal void follow(string username, string project)
+        {
+            addFollow(username, project);
+        }
+
+        private void addFollow(string username, string project)
+        {
+            if (userProjects.ContainsKey(username))
+            {
+                if (!userProjects[username].Contains(project))
+                {
+                    userProjects[username].Add(project);
+                }
+            }
+            else
+            {
+                userProjects.Add(username, new List<string>());
+                userProjects[username].Add(project);
+            }
+
+            userFollowProjectsLogs[username] = projectLogs[project];
+        }
+
+        internal void post(string username, string project, string message)
+        {
+            Log newLog = new Log(username, project, message);
+            messageLog.Add(newLog);
+            prepareViews(newLog);
+        }
+
+        private void prepareViews(Log newLog)
+        {
+            String project = newLog.GetProject();
+            if (!projectLogs.ContainsKey(project)) 
+            {
+                projectLogs[project] = new List<Log> { newLog };
+            }
+            else
+            {
+                projectLogs[project].Add(newLog);
+            }
+        }
+
+        /*
+            Alice
+            I'm working on the login screen (5 minutes ago)
+            Bob
+            Awesome, I'll start on the forgotten password screen (4 minutes ago)
+            I'll give you the link to put on the log on screen shortly Alice (1 minute ago)
+        */
+        internal string read(string project)
+        {
+            string result = "";
+            foreach (Log log in projectLogs[project])
+            {
+                result += log.GetUsername() + "\n" + log.GetMessage() + " " + getMinutesString(DateTime.Now - log.GetTime()) + "\n"; 
+            }
+            return result;
+        }
+
+        private string getMinutesString(TimeSpan timeSpan)
+        {
+            string result =  "(" + timeSpan.TotalMinutes + " minutes ago)";
+            return result;
+        }
+
+        // Charlie wall
+        // Apollo - Bob: Has anyone thought about the next release demo? (6 minutes ago)
+        internal String wall(string username)
+        {
+            string result = "";
+            foreach (List<Log> projectList in userFollowProjectsLogs.Values)
+            {
+                foreach (Log log in projectList)
+                {
+                    result += log.GetProject() + " - " + log.GetUsername() + ": " + log.GetMessage() + 
+                        getMinutesString(DateTime.Now - log.GetTime()) + "\n";
+                }
+            }
+            return result;
+        }
+
+    }
+
+    internal class Log
+    {
+        private String username;
+        private String project;
+        private String message;
+        private DateTime logTime;
+        internal Log(String username, String project, String message) 
+        {
+            this.username = username;
+            this.message = message;
+            this.project = project;
+            logTime = DateTime.Now;
+        }
+        public String GetUsername()
+        {
+            return username;
+        }
+        public String GetProject()
+        {
+            return project;
+        }
+        public String GetMessage()
+        {
+            return message;
+        }
+        public DateTime GetTime()
+        {
+            return logTime;
+        }
+    }
+}
---
+++ inLogikInterview/Program.cs
@@ -0,0 +1,66 @@
+﻿//Assumptions
+//I assume shouldn't use tools like Kafka and I should implement my event log system myself
+//project names and usernames are one syllubes only
+//You didn't intend me to asynchornously create views on the data for the read parts as I have to deal with thread safety and all!
+//I'll try to implement threads if I have extra time
+//As the read query is user agnostic, the seen logs can't be omitted for the next time that someone asks for them. For the wall command
+//this feature can be implememnted that after seeing logs, don't show them the next time. I'll implement this if I have time
+
+using inLogikInterview;
+using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            string query = "";
+            query = Console.ReadLine();
+            handleQuery(query);
+        }
+
+
+        /*
+        posting: < user name > -> @< project name > < message >
+        reading: < project name >
+        following: < user name > follows < project name >
+        wall: < user name > wall
+        */
+
+
+
+        void handleQuery(string query)
+        {
+            string username = "";
+            string project = "";
+            string message = "";
+            LogManager logManager = new LogManager();
+            // Alice -> @Moonshot I'm working on the log on screen
+            if (query.Contains("->"))
+            {
+                username = query.Split(" ")[0];
+                query = query.Substring(username.Length + 5);
+                project = query.Split(" ")[0];
+                message = query.Substring(username.Length + 1);
+                logManager.post(username, project, message);
+            }
+            else if (query.Split(' ').Length == 1)
+            {
+                project = query;
+                Console.WriteLine(logManager.read(project));
+            }
+            else if (query.Split(' ').Length == 3)
+            {
+                username = query.Split(' ')[0];
+                project = query.Split(" ")[2];
+                logManager.follow(username, project);
+            }
+            else
+            {
+                username = query.Split(' ')[0];
+                Console.WriteLine(logManager.wall(username));
+            }
+        }
+    }
+}
